5859be847d9e876669a0960cdab4a431fe59acd2
serieznyi <serieznyi@gmail.com>
2013-03-11 11:13:45 +0200

    add panel view

--- src/myconnector/client/ViewPanel.java
+++ src/myconnector/client/ViewPanel.java
@@ -1,27 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package myconnector.client;
 
 import java.awt.Toolkit;
 import javax.swing.JPanel;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 /**
  *
  * @author serieznyi
  */
-public class ViewFrame extends javax.swing.JFrame {
+public class ViewPanel extends javax.swing.JPanel {
 
     /**
-     * Creates new form ViewFrame
+     * Creates new form ViewPanel
      */
-    public ViewFrame() {
+    public ViewPanel() {
         initComponents();
         
-        //this.setBounds(25, 25, 400, 400);
         Toolkit tk = Toolkit.getDefaultToolkit();  
         int xSize = ((int) tk.getScreenSize().getWidth());  
         int ySize = ((int) tk.getScreenSize().getHeight());  
@@ -40,22 +37,19 @@ public class ViewFrame extends javax.swing.JFrame {
 
         jp_reciever = new javax.swing.JPanel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBounds(new java.awt.Rectangle(0, 0, 400, 400));
-
         javax.swing.GroupLayout jp_recieverLayout = new javax.swing.GroupLayout(jp_reciever);
         jp_reciever.setLayout(jp_recieverLayout);
         jp_recieverLayout.setHorizontalGroup(
             jp_recieverLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 562, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         jp_recieverLayout.setVerticalGroup(
             jp_recieverLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 397, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jp_reciever, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -64,13 +58,7 @@ public class ViewFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jp_reciever, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jp_reciever;
     // End of variables declaration//GEN-END:variables
